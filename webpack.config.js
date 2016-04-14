d0859df2519e83354ac2601b03abdc042a763739
anders462 <anders462@gmail.com>
2016-04-14 11:49:25 -0700

    problems with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-
+
+  webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
@@ -14,7 +16,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loade
+        r"},
       {test: /\.css$/, loader: "style-loader!css-loader"}
     ]
   },
